1465b2353ac14bc1aef725c5d8bfa3a1344ba28d
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-08 11:30:38 -0300

    add categories store & api

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const API_BASE = 'http://localhost:3000/api'
 export const API_ROUTES = {
   profile: '/profile',
+  categories: '/categories',
 }
 
 export const http = axios.create({
---
+++ src/store/category.store.ts
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { ICategory } from '@/interfaces/category.interface'
+import { API_ROUTES, http } from '@/api'
+
+export const useCategoryStore = defineStore('category', () => {
+  const categories = ref<ICategory[]>([])
+
+  async function getCategories() {
+    const { data } = await http.get(API_ROUTES.categories)
+    categories.value = data
+  }
+
+  return { categories, getCategories }
+})
