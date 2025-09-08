52e96047a7987e480fd49459d45f57c916b6a096
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-08 11:30:16 -0300

    add axios usage

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,11 @@
+import axios from 'axios'
+
 export const API_BASE = 'http://localhost:3000/api'
 export const API_ROUTES = {
-  profile: `${API_BASE}/profile`,
+  profile: '/profile',
 }
+
+export const http = axios.create({
+  baseURL: API_BASE,
+  timeout: 10000,
+})
--- src/store/profile.store.ts
+++ src/store/profile.store.ts
@@ -1,15 +1,14 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { IProfile } from '@/interfaces/profile.interface'
-import { API_ROUTES } from '@/api'
+import { API_ROUTES, http } from '@/api'
 
 export const useProfileStore = defineStore('profile', () => {
   const profile = ref<IProfile>()
 
   async function getProfile() {
-    const data = await fetch(API_ROUTES.profile)
-    const result = (await data.json()) as IProfile
-    profile.value = result
+    const { data } = await http.get<IProfile>(API_ROUTES.profile)
+    profile.value = data
   }
 
   return { profile, getProfile }
