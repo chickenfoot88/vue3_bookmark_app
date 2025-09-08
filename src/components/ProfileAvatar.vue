e6afff68439a7687d0d3c72ac40d660aa7abe178
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-08 09:53:57 -0300

    add profile avatar component & image

---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="profile-avatar">
+    <img src="/public/avatar.png" alt="Изображение пользователя" class="profile-avatar-image" />
+    <div>
+      Привет, <strong>{{ userName }}</strong>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { userName } = defineProps<{ userName: string }>()
+// defineEmits<>()
+</script>
+
+<style scoped>
+.profile-avatar {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  font-size: 18px;
+}
+
+.profile-avatar-image {
+  width: 80px;
+}
+</style>
