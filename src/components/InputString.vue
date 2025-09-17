9f83eca243cc67e03de3ebabdb5a355e16299d66
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-17 08:36:54 -0300

    add input component

---
+++ src/components/InputString.vue
@@ -0,0 +1,32 @@
+<template>
+  <input class="input-string" v-model="data" ref="input" :type />
+</template>
+
+<script lang="ts" setup>
+import { onMounted, useTemplateRef } from 'vue'
+
+const data = defineModel<string>()
+const input = useTemplateRef<HTMLInputElement>('input')
+
+const { isFocused = false, type = 'text' } = defineProps<{ isFocused?: boolean; type?: string }>()
+
+onMounted(() => {
+  if (isFocused && input.value) {
+    input.value.focus()
+  }
+})
+</script>
+
+<style scoped>
+.input-string {
+  border: none;
+  border-bottom: 1px solid var(--color-fg);
+  color: var(--color-fg);
+  font-size: 24px;
+  outline: none;
+}
+.input-string::placeholder {
+  color: var(--color-inactive);
+  font-size: 18px;
+}
+</style>
