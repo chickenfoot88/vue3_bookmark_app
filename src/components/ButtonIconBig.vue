7b31191d066d108dcc4b3b5dbba21a85e1dc123d
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-18 11:19:11 -0300

    add button big component

---
+++ src/components/ButtonIconBig.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="button-icon-big">
+    <slot />
+  </button>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+.button-icon-big {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  cursor: pointer;
+  width: 48px;
+  height: 48px;
+  border-radius: 50px;
+  border: 3px solid var(--color-bg);
+}
+.button-icon:hover {
+  background: var(--color-bg-hover);
+}
+</style>
