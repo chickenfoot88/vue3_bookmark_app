f0ecab6a8b9fa39e93bb93a7897c6216093b08c6
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-18 09:52:50 -0300

    add logout button

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -12,6 +12,7 @@
   <ButtonIcon @click="store.createCategory">
     <IconPlus />
   </ButtonIcon>
+  <a class="logout-link" @click="logout">Выход</a>
 </template>
 
 <script setup lang="ts">
@@ -19,12 +20,21 @@ import { useCategoryStore } from '@/store/category.store'
 import { onMounted } from 'vue'
 import ButtonIcon from '@/components/ButtonIcon.vue'
 import IconPlus from '@/icons/IconPlus.vue'
+import { useAuthStore } from '@/store/auth.store'
+import { useRouter } from 'vue-router'
 
 const store = useCategoryStore()
+const authStore = useAuthStore()
+const router = useRouter()
 
 onMounted(() => {
   store.getCategories()
 })
+
+function logout() {
+  authStore.clearToken()
+  router.push({ name: 'auth' })
+}
 </script>
 
 <style scoped>
@@ -52,5 +62,15 @@ onMounted(() => {
 .category-list-item a.active-link {
   font-size: 24px;
   font-weight: 700;
+}
+
+.logout-link {
+  cursor: pointer;
+  margin-top: 30px;
+  display: block;
+}
+
+.logout-link:hover {
+  text-decoration: underline;
 }
 </style>
